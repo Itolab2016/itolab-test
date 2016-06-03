c0115fecf8556f9a4e871bf18f07ad682b5a9a84
Kouhei <kouhei.ito@itolab-ktc.com>
2016-06-03 15:26:13 +0900

    Update hoge.c

--- hoge.c
+++ hoge.c
@@ -9,12 +9,12 @@ double toma(double, double);
 double kitayama(double, double);
 
 main(){
-  a=sawada(1.0,2.0);
+  a=1.0;//sawada(1.0,2.0);
   b=ichimura(a, 3.0);
   c=sasai(b, 4.0);
   d=hatake(c, 5.0);
   e=toma(d, 6.0);
-  f=kenzaki(e, 7.0);
-  g=kitayama(f, 8.0);
+  //f=kenzaki(e, 7.0);
+  g=kitayama(e, 8.0);
   printf("%f\n",g);
 }
